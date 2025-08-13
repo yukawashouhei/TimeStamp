16b5344a5f7078b7a40791fa02a8bffbcc065e0b
Shohei <yukawa.shouhei@gmail.com>
2025-08-13 12:03:40 +0900

    Initial Commit

---
+++ TimeStamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeStamp
+//
+//  Created by 湯川昇平 on 2025/08/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeStamp/TimeStampApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeStampApp.swift
+//  TimeStamp
+//
+//  Created by 湯川昇平 on 2025/08/13.
+//
+
+import SwiftUI
+
+@main
+struct TimeStampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
